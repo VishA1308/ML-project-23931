3700f0ecd6543fe70850730297a428a857f6b9d3
ybasova <y.basova@g.nsu.ru>
2025-03-06 12:27:54 +0700

    updated tesseract_test.py

--- tesseract_test.py
+++ tesseract_test.py
@@ -1,8 +1,22 @@
 import pytesseract
 from PIL import Image
+import cv2
 
+from preprocess_of_image import make_image, analyze_image, get_parametres
 
-image = Image.open('vitD.png')
-string = pytesseract.image_to_string(image, lang='rus', config='--psm 6')
+def get_text(image_path):
+    image = cv2.imread(image_path, 0) # read image in gray-scale
+    image = Image.fromarray(image)
 
-print(string)
+    mean_bright, std_bright= get_parametres(image)
+    contr_adj, bright_adj, adj_type = analyze_image(mean_bright, std_bright)
+
+    processed_image = make_image(image, adj_type, 
+                                 bright_adj, 
+                                 contr_adj)
+    string = pytesseract.image_to_string(processed_image, 
+                                         lang='rus+eng',
+                                         config='--psm 6')
+    
+    return string
+
