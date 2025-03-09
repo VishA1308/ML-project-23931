88da534b67e3992425d02b4588ecaf2960052216
ybasova <y.basova@g.nsu.ru>
2025-03-09 15:21:48 +0700

    updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from tesseract_test import get_text
 from ustranenie_in_text import filter_text
 
-image_path = 'templates/vitD.jpg'
+image_path = 'templates/esenin.jpg'
 
-result_text = get_text(image_path)
+result_text = get_text(image_path) # processing of image, string in result
 
 print(result_text)
+print(filter_text(result_text)) # filter text from extra symbols
