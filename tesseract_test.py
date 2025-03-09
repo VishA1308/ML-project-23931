1097a95968539e001347f4f4385c0ddfb48bfd9a
ybasova <y.basova@g.nsu.ru>
2025-03-09 15:20:53 +0700

    updated tesseract_test.py

--- tesseract_test.py
+++ tesseract_test.py
@@ -4,19 +4,27 @@ import cv2
 
 from preprocess_of_image import make_image, analyze_image, get_parametres
 
-def get_text(image_path):
+def get_text(image_path: str) -> str:
+    """
+    Recognises text on the image and returns a result string.
+    
+    image_text - path to the image in string format.
+    """ 
     image = cv2.imread(image_path, 0) # read image in gray-scale
-    image = Image.fromarray(image)
+    image = Image.fromarray(image) # convertation for further processing
 
+    # Extracting parameters for preprocessing
     mean_bright, std_bright= get_parametres(image)
     contr_adj, bright_adj, adj_type = analyze_image(mean_bright, std_bright)
 
+    # Preprocessing
     processed_image = make_image(image, adj_type, 
                                  bright_adj, 
                                  contr_adj)
+    
+    # Implementing pyteseract to recognise text
     string = pytesseract.image_to_string(processed_image, 
                                          lang='rus+eng',
                                          config='--psm 6')
     
     return string
-
