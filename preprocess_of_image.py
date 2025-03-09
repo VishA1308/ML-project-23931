35dadd4722cb490beea4a3602d0dfbc2dc69d4b3
Marina <m.zhiltsova@g.nsu.ru>
2025-03-09 17:30:12 +0700

    preprocess_of_image.py

--- preprocess_of_image.py
+++ preprocess_of_image.py
@@ -1,105 +1,93 @@
 from PIL import Image, ImageEnhance
 import numpy as np
-import cv2 as cv
+import cv2
+import os
 
 def get_parametres(img):
-    img_array = np.array(img)
-    mean_brightness = np.mean(img_array)
-    std_brightness = np.std(img_array)
-    
-    return mean_brightness, std_brightness
+    img = np.array(img)
+    img_mean = np.mean(img)
+    img_std = np.std(img)
+
+    return img_mean, img_std
+
+def calculate_temp(contrast):
+    return (1 + contrast * 5 / 130)
+
+def adaptive_bilateral_filter(image):
+    height, width = image.shape[:2]
+    std_color = np.std(image, axis=(0, 1))
+    avg_std_color = np.mean(std_color)
+    contrast = np.std(image)
+    temp = calculate_temp(contrast)
+    d = int(max(5, min(width, height) // 10) / temp)
+    sigmaColor = int(max(10, int(avg_std_color * 2)) / temp)
+    sigmaSpace = int(max(5, min(width, height) // 20) / temp)
+    filtered_image = cv2.bilateralFilter(image, d=d, sigmaColor=sigmaColor, sigmaSpace=sigmaSpace)
+    return filtered_image
 
 def analyze_image(mean_brightness, std_brightness):
     flag_brightness = 0
     flag_contrast = 0
-    adjust_type = 0
+    
+    # Корректировка яркости
     if mean_brightness < 100:
-        brightness_adjustment = 2  
-        adjust_type = 1
+        brightness_adjustment = 1 + (50 - mean_brightness) / 100  # Увеличиваем яркость
         flag_brightness = 1
-    elif mean_brightness > 150:
-        brightness_adjustment = 0.5
-        adjust_type = 1
+    elif mean_brightness > 250:
+        brightness_adjustment = 1 - (mean_brightness - 250) / 100  # Уменьшаем яркость
         flag_brightness = 1
     else:
         brightness_adjustment = 1
         
-    
+    # Корректировка контрастности
     if std_brightness < 50:
-        contrast_adjustment = 2  
-        adjust_type = 2
+        contrast_adjustment = 1 + (50 - std_brightness) / 100  # Увеличиваем контрастность
         flag_contrast = 1
     elif std_brightness > 150:
-        contrast_adjustment = 0.5  
-        adjust_type = 2
+        contrast_adjustment = 1 - (std_brightness - 150) / 100  # Уменьшаем контрастность
         flag_contrast = 1
     else:
         contrast_adjustment = 1
 
-    if (flag_contrast == 1 and flag_brightness == 1):
+    adjust_type = 0
+    if flag_contrast and flag_brightness:
         adjust_type = 3
+    elif flag_brightness:
+        adjust_type = 1
+    elif flag_contrast:
+        adjust_type = 2
 
     return contrast_adjustment, brightness_adjustment, adjust_type
 
-def adaptive_bilateral_filter(image):
-    height, width = image.shape[:2]
-    num_pixels = height * width
+def make_image(img, adjust_type=0, brightness=1, contrast=1):
+    brightness = max(0.5, min(brightness, 1.5))
+    contrast = max(0.5, min(contrast, 1.5))
 
-    mean_color = np.mean(image, axis=(0, 1))
-    std_color = np.std(image, axis=(0, 1))
-    avg_std_color = np.mean(std_color)
+    if adjust_type == 1:  
+        img = ImageEnhance.Brightness(img).enhance(brightness)
 
-    d = max(5, min(width, height) // 10)
-    sigmaColor = int(avg_std_color * 2)
-    sigmaSpace = max(5, min(width, height) // 20)
+    elif adjust_type == 2:  
+        img = ImageEnhance.Contrast(img).enhance(contrast)
 
-    filtered_image = cv.bilateralFilter(image, d=d, 
-                                        sigmaColor=sigmaColor, 
-                                        sigmaSpace=sigmaSpace)
-
-    return filtered_image
-
-
-def make_image(img, adjust_type=0,brightness_adjustment=1, contrast_adjustment=1):
-    if adjust_type == 1:  # Изменить только яркость
-        img = ImageEnhance.Brightness(img).enhance(brightness_adjustment)
-        img.save(f'Brightness-{contrast_adjustment}.jpg')
-    elif adjust_type == 2:  # Изменить только контрастность
-        img = ImageEnhance.Contrast(img).enhance(contrast_adjustment)
-        img.save(f'Contrast-{contrast_adjustment}.jpg')
-    elif adjust_type == 3:  # Изменить и яркость, и контрастность
-        img_brightness = ImageEnhance.Brightness(img).enhance(brightness_adjustment)
-        img = ImageEnhance.Contrast(img_brightness).enhance(contrast_adjustment)
-        img.save(f'Contrast_and_Brightness-{contrast_adjustment}.jpg')
+    elif adjust_type == 3: 
+        img_brightness = ImageEnhance.Brightness(img).enhance(brightness)
+        img = ImageEnhance.Contrast(img_brightness).enhance(contrast)
     else:
         print("Изменения не нужны")
-        return img
-        
+    
     img = np.array(img)
-    img = adaptive_bilateral_filter(img)
-
+    img = adaptive_bilateral_filter(img) 
     return img
 
+
 '''
-input_image_path = 'templates/esenin.jpg'
+files = os.listdir('templates/')
+os.makedirs('res/', exist_ok=True)
 
-image_orig = Image.open(input_image_path)
-image_orig.save('original_image.jpg')
-print('Оригинальное изображение сохранено как "original_image.jpg"\n')
-
-image_path = input_image_path
-image_cv2 = cv.imread(image_path)
-gray_image = cv.cvtColor(image_cv2, cv.COLOR_BGR2GRAY)
-img = Image.fromarray(gray_image)
-
-
-mean_brightness, std_brightness= get_parametres(img)
-print(f"Средняя яркость: {mean_brightness}, \
-      Стандартное отклонение яркости: {std_brightness}\n")
-
-contrast_adjustment, brightness_adjustment, adjust_type = analyze_image(mean_brightness, std_brightness)
-
-res_img = make_image(img, adjust_type=adjust_type,
-                     contrast_adjustment=contrast_adjustment,
-                     brightness_adjustment=brightness_adjustment)
+for file in files:
+    image = cv2.imread(f'templates/{file}', cv2.IMREAD_GRAYSCALE)
+    img_mean, img_std = get_parameters_from_image(image)
+    contrast, brightness, adjust_type = analyze_image(img_mean, img_std)
+    res_img = make_image(image, adjust_type=adjust_type, contrast=contrast, brightness=brightness)
+    cv2.imwrite(f'res/{file}', res_img)
 '''
