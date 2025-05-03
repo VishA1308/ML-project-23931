4d52ea5d4d2bc524d87c819b5918f807e6b9cde9
ybasova <y.basova@g.nsu.ru>
2025-05-03 12:57:01 +0700

    updated main.py

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
+import json
+
 from tesseract_test import get_text
 from ustranenie_in_text import filter_text
-from converter_pdf import convert_to_png
-import os
 
-image_path = r'C:\\Project ML\\test main\\image'  
-output_directory = r'C:\\Project ML\\test main\\img_png' 
+image_path = 'templates/vitD.jpg'
 
+rec_text = get_text(image_path)
+filt_text = filter_text(rec_text)
 
-convert_to_png(image_path, output_directory)
+response_dict = {"text": filt_text}
 
-# Обработка изображений в выходной директории
-for image in os.listdir(output_directory):
-
-    image_full_path = os.path.join(output_directory, image)
-    
-    # Проверяем, что файл является изображением
-    if image.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp')):
-        result_text = get_text(image_full_path)  
-        print(result_text)
-        print(filter_text(result_text))
-
+with open('response.json', 'w+') as file:
+    json.dump(response_dict, file)
