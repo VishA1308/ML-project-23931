81565080adc513c76e7beed3d1355ff19f5c8197
Marina Zhiltsova <m.zhiltsova@g.nsu.ru>
2025-05-18 14:15:40 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import json
 from typing import Annotated
 from tesseract_test import get_text
 from ustranenie_in_text import filter_text
+from extraction import nlp_extraction
 from fastapi import FastAPI, HTTPException, File, UploadFile
 import tempfile
 import os
@@ -22,6 +23,9 @@ async def process(file: UploadFile):
             filt_text = filter_text(rec_text)
 
             response_dict = {"rec_text": rec_text, "text": filt_text}
+
+            res = nlp_extraction(filt_text)
+
             
             return response_dict
         finally:
